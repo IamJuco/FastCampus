7befe98af7fdba78c7e8a0553af62421c0eefe18
Juseonghyun <wntjdgus99@naver.com>
2024-07-28 15:56:58 +0900

    chapter8 - 권한설정 ( 버전마다 )

--- part1/chapter8/app/src/main/java/com/example/chapter8/MainActivity.kt
+++ part1/chapter8/app/src/main/java/com/example/chapter8/MainActivity.kt
@@ -1,20 +1,151 @@
 package com.example.chapter8
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.chapter8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        uiSetting()
+
+        binding.btnLoadImage.setOnClickListener {
+            checkPermission()
+        }
+
+    }
+
+    private fun uiSetting() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun checkPermission() {
+        when {
+            // permission 체크
+            // Android14 && SDK 34 이상
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE && ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                loadImage()
+            }
+
+            // Android 13 && SDK 33
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_MEDIA_IMAGES
+            ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_MEDIA_VIDEO
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                loadImage()
+            }
+
+            // Android 12 && SDK 32 이하
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                loadImage()
+            }
+
+            // 권한을 거부 했을 때
+            shouldShowRequestPermissionRationale(
+                android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED) ||
+            shouldShowRequestPermissionRationale(
+                android.Manifest.permission.READ_MEDIA_IMAGES
+            ) ||
+            shouldShowRequestPermissionRationale(
+                android.Manifest.permission.READ_MEDIA_VIDEO
+            ) ||
+            shouldShowRequestPermissionRationale(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) -> {
+                showPermissionInfoDialog()
+            }
+
+            // 권한이 없을 때
+            else -> {
+                requestReadExternalStorage()
+            }
+
+        }
+    }
+
+    private fun loadImage() {
+        Toast.makeText(this, "이미지를 가져올 예정", Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun showPermissionInfoDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("이미지를 가져오기 위해서 외부 저장소 읽기 권한이 필요 합니다.")
+            setNegativeButton("취소", null)
+            setPositiveButton("동의") { _, _ ->
+                requestReadExternalStorage()
+            }
+        }.show()
+    }
+
+    private fun requestReadExternalStorage() {
+        when {
+            // permission 요청
+            // Android14 && SDK 34 이상
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE -> {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.READ_MEDIA_IMAGES,
+                        android.Manifest.permission.READ_MEDIA_VIDEO,
+                        android.Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+                    ),
+                    REQUEST_READ_EXTERNAL_STORAGE
+                )
+            }
+            // Android 13 && SDK 33
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.READ_MEDIA_IMAGES,
+                        android.Manifest.permission.READ_MEDIA_VIDEO,
+                    ),
+                    REQUEST_READ_EXTERNAL_STORAGE
+                )
+            }
+            // Android 12 && SDK 32 이하
+            else -> {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    ),
+                    REQUEST_READ_EXTERNAL_STORAGE
+                )
+            }
+        }
+    }
+
+    companion object {
+        // 권한 요청에 대한 요청 값: 100 ( 키값 )
+        const val REQUEST_READ_EXTERNAL_STORAGE = 100
     }
 }
