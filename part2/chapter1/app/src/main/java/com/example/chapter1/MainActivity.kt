aa9c2904e1a6710a31944fb7c2cd95df81582efa
Juseonghyun <wntjdgus99@naver.com>
2024-07-31 21:19:23 +0900

    chapter1 - 02.webView 사용해보기

--- part2/chapter1/app/src/main/java/com/example/chapter1/MainActivity.kt
+++ part2/chapter1/app/src/main/java/com/example/chapter1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.chapter1
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        uiSetting()
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        // XSS 취약성 ( 외부로부터 공격 받을 수 있음, 무시 해도 됌 )
+        webView.settings.javaScriptEnabled = true
+
+        webView.loadUrl("https://google.com")
+    }
+
+    private fun uiSetting() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
