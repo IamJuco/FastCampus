e8326d3a25d1d1cb7b54ec274f1515c8c808ee94
IamJuco <jucoding99@gmail.com>
2024-10-22 18:12:44 +0900

    02. Text

--- part4/chapter3/app/src/main/java/com/nemocompany/chapter3/MainActivity.kt
+++ part4/chapter3/app/src/main/java/com/nemocompany/chapter3/MainActivity.kt
@@ -6,11 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nemocompany.chapter3.ui.theme.Chapter3Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +28,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Chapter3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier.size(300.dp),
+        text = "Hello $name\nHello $name\nHello $name",
+        color = Color(0xffff9944),
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold, // textStyle
+        fontFamily = FontFamily.Monospace, // font
+        letterSpacing = 2.sp, // 단어 하나하나 마다 간격을 벌려줌
+        textDecoration = TextDecoration.Underline,
+        textAlign = TextAlign.Center, // textGravity
     )
 }
 
