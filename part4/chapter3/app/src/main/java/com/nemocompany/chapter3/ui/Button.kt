c15ce04d56401577a684cc16925107305e3a0a13
IamJuco <jucoding99@gmail.com>
2024-10-25 19:11:27 +0900

    03. Button - vector 이미지추가

--- part4/chapter3/app/src/main/java/com/nemocompany/chapter3/ui/Button.kt
+++ part4/chapter3/app/src/main/java/com/nemocompany/chapter3/ui/Button.kt
@@ -1,6 +1,10 @@
 package com.nemocompany.chapter3.ui
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,5 +13,8 @@ import androidx.compose.ui.Modifier
 fun ButtonExample(modifier: Modifier = Modifier, onButtonClicked: () -> Unit) {
     Button(onClick = onButtonClicked, modifier = modifier) {
         Text(text = "Send")
+        // Vector 이미지를 버튼에 추가
+        // contentDescription -> 보통 시각장애인 떄문에 생김
+        Icon(imageVector = Icons.Filled.Send, contentDescription = null)
     }
 }
