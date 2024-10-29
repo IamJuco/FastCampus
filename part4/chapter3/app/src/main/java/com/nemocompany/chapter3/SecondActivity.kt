1dcd28b9abaef43f6492c78cbcb371d7bf58520b
IamJuco <jucoding99@gmail.com>
2024-10-30 00:33:17 +0900

    학습용 Activity생성

---
+++ part4/chapter3/app/src/main/java/com/nemocompany/chapter3/SecondActivity.kt
@@ -0,0 +1,43 @@
+package com.nemocompany.chapter3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nemocompany.chapter3.ui.main.ColumnExample
+import com.nemocompany.chapter3.ui.theme.Chapter3Theme
+
+class SecondActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Chapter3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingSecondPreview() {
+    Chapter3Theme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(modifier = Modifier.padding(innerPadding)) {
+
+            }
+        }
+    }
+}
