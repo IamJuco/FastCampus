c01f0208767d469218172c78c3cfbacfc49783c6
IamJuco <jucoding99@gmail.com>
2024-11-01 02:43:41 +0900

    03. ConstraintLayout

--- part4/chapter4/app/src/main/java/com/nemocompany/chapter4/MainActivity.kt
+++ part4/chapter4/app/src/main/java/com/nemocompany/chapter4/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,28 +22,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             Chapter4Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Chapter4Theme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(modifier = Modifier.padding(innerPadding)) {
+
+            }
+        }
     }
 }
---
+++ part4/chapter4/app/src/main/java/com/nemocompany/chapter4/ui/ConstraintLayout.kt
@@ -0,0 +1,8 @@
+package com.nemocompany.chapter4.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ConstraintLayoutExample() {
+
+}
