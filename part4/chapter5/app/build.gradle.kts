009554d5d57ace25f9e3e2dfff76911feacf8a04
IamJuco <jucoding99@gmail.com>
2024-11-07 17:54:50 +0900

    03. LiveData - implementation 추가

--- part4/chapter5/app/build.gradle.kts
+++ part4/chapter5/app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
 
     // ViewModel Compose
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
