31763aa329c574c2cf3c976c4d3064c221e77cc8
IamJuco <jucoding99@gmail.com>
2024-11-08 00:02:00 +0900

    07. 의존성 주입 - Hilt와 ksp implementation추가

--- part4/chapter5/app/build.gradle.kts
+++ part4/chapter5/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -58,6 +60,9 @@ dependencies {
     implementation(libs.androidx.runtime.livedata)
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.8.3")
+    // Hilt & ksp
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
