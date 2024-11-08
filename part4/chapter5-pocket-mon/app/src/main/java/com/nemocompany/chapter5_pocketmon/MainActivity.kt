f155d257a696e361ee1e739f2b356394af8409d3
IamJuco <jucoding99@gmail.com>
2024-11-08 19:42:59 +0900

    09. 포켓몬 앱

--- part4/chapter5-pocket-mon/app/src/main/java/com/nemocompany/chapter5_pocketmon/MainActivity.kt
+++ part4/chapter5-pocket-mon/app/src/main/java/com/nemocompany/chapter5_pocketmon/MainActivity.kt
@@ -7,12 +7,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.nemocompany.chapter5_pocketmon.screen.DetailScreen
+import com.nemocompany.chapter5_pocketmon.screen.MainScreen
 import com.nemocompany.chapter5_pocketmon.ui.theme.Chapter5PocketMonTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Chapter5PocketMonTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TopLevel(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +36,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TopLevel(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        "Home",
         modifier = modifier
-    )
-}
+    ) {
+        composable("Home") {
+            MainScreen(
+                onPokemonClick = {
+                    val pokemonId = it.substringAfter("pokemon/")
+                        .substringBefore("/")
+                        .toInt()
+                    navController.navigate("Detail/${pokemonId}")
+                }
+            )
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Chapter5PocketMonTheme {
-        Greeting("Android")
+        composable(
+            "Detail/{pokemonId}",
+            arguments = listOf(
+                navArgument("pokemonId") {
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            val pokemonId = it.arguments?.getInt("pokemonId") as Int
+            DetailScreen(
+                pokemonId = pokemonId,
+                onUpButtonClick = {
+                    navController.navigate("Home") {
+                        popUpTo("Home") {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
     }
 }
