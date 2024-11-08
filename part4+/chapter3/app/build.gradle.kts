847c8e1156ff7dfffa9550ea128696e2b2122ac2
IamJuco <jucoding99@gmail.com>
2024-11-08 23:37:19 +0900

    sdk 35로 변경

--- part4+/chapter3/app/build.gradle.kts
+++ part4+/chapter3/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.nemocompany.chapter3movieapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.nemocompany.chapter3movieapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
