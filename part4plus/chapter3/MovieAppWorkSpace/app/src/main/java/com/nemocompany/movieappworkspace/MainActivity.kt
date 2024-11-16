4ded33f478501d4d4d67b2d95d2061041bf26014
IamJuco <jucoding99@gmail.com>
2024-11-16 15:09:48 +0900

    14. 네비게이션 라이브러리 설정 - Compose Empty View로 프로젝트를 시작했는데 MainActivity를 AppCompatActivity로 바꾸면서 Themes 값을 변경 해줘야함

--- part4plus/chapter3/MovieAppWorkSpace/app/src/main/java/com/nemocompany/movieappworkspace/MainActivity.kt
+++ part4plus/chapter3/MovieAppWorkSpace/app/src/main/java/com/nemocompany/movieappworkspace/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -15,41 +16,10 @@ import com.nemocompany.movieappworkspace.ui.components.movie.MovieItem
 import com.nemocompany.movieappworkspace.ui.components.buttons.PrimaryButton
 import com.nemocompany.movieappworkspace.ui.theme.MovieAppWorkSpaceTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            MovieAppWorkSpaceTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-                    PrimaryButton(
-                        text = "Submit",
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieAppWorkSpaceTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
