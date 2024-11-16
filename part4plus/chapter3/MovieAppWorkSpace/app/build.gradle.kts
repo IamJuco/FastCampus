90431ef35c6a1633496a0cf105ca5de4200f46ac
IamJuco <jucoding99@gmail.com>
2024-11-16 16:41:40 +0900

    15. 영화피드(DI와 레트로핏 설정) - okhttp3:logging-interceptor추가

--- part4plus/chapter3/MovieAppWorkSpace/app/build.gradle.kts
+++ part4plus/chapter3/MovieAppWorkSpace/app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
 
     // OkHttp3
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     // Gson
     implementation("com.google.code.gson:gson:2.11.0")
