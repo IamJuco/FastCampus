3cf9e279b52942483b31e2ed74bd67a4039af345
IamJuco <jucoding99@gmail.com>
2024-11-16 15:16:11 +0900

    15. 영화피드(DI와 레트로핏 설정) - enableEdgeToEdge에 의한 uiSetting추가

--- part4plus/chapter3/MovieAppWorkSpace/app/src/main/java/com/nemocompany/movieappworkspace/MainActivity.kt
+++ part4plus/chapter3/MovieAppWorkSpace/app/src/main/java/com/nemocompany/movieappworkspace/MainActivity.kt
@@ -12,14 +12,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.nemocompany.movieappworkspace.ui.components.movie.MovieItem
 import com.nemocompany.movieappworkspace.ui.components.buttons.PrimaryButton
 import com.nemocompany.movieappworkspace.ui.theme.MovieAppWorkSpaceTheme
 
+//Compose Empty View로 프로젝트를 시작했는데 MainActivity를
+// AppCompatActivity로 바꾸면서 Themes 값을 변경 해줘야함
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        uiSetting()
+    }
+
+    private fun uiSetting() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(insets.left, insets.top, insets.right, insets.bottom)
+            WindowInsetsCompat.CONSUMED
+        }
     }
 }
